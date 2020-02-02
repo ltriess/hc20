8d7ce3d969fb31679e3ea2982c2e16caa1230083
Larissa Triess <mail@triess.eu>
2020-02-02 16:34:05 +0100

    Visualize output

--- src/visualize.py
+++ src/visualize.py
@@ -3,24 +3,40 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from src.common import load_output, load
+from src.common import load
 from src.solution_dummy import solution_dummy
 
 
-def plot_pool_matrix(server_dict):
-    pass
+def plot_pool_matrix(server_dict, matrix):
+
+    matrix = np.int32(matrix)
+    for server in server_dict:
+
+        for i in range(server["size"]):
+            matrix[server["row"]][server["left_slot"] + i] = server["pool_id"]
+
+    plt.imshow(matrix)
+    plt.show()
 
 
-def plot_capacity_matrix(server_dict):
-    pass
+def plot_capacity_matrix(server_dict, matrix):
+
+    matrix = np.int32(matrix)
+    for server in server_dict:
+
+        for i in range(server["size"]):
+            matrix[server["row"]][server["left_slot"] + i] = server["capacity"]
+
+    plt.imshow(matrix)
+    plt.show()
 
 
 def main():
     available, servers, p = load(example=False)
     server_dict = solution_dummy(available, servers, p)
 
-    plot_capacity_matrix(server_dict)
-    plot_pool_matrix(server_dict)
+    plot_capacity_matrix(server_dict, available)
+    plot_pool_matrix(server_dict, available)
 
 
 if __name__ == "__main__":
