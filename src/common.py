11640a61fc3739594c86b98e86de90bf164150c2
DavidS3141 <davidschmidt314@gmail.com>
2020-02-02 13:14:25 +0100

    implement saver

--- src/common.py
+++ src/common.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 
+import os.path as osp
+import time
+
 import numpy as np
 import pathlib
+
+
+def get_time_stamp(with_date=True, with_delims=False):
+    if with_date:
+        if with_delims:
+            return time.strftime("%Y/%m/%d-%H:%M:%S")
+        else:
+            return time.strftime("%Y%m%d-%H%M%S")
+    else:
+        if with_delims:
+            return time.strftime("%H:%M:%S")
+        else:
+            return time.strftime("%H%M%S")
 
 
 def load(example=True):
@@ -27,5 +43,33 @@ def load(example=True):
     return available, servers, p
 
 
+def save(server_allocation, output_name="example"):
+    m = len(server_allocation)
+    assert m >= 1
+    assert sorted(server_allocation[0].keys()) == ["left_slot", "pool_id", "row"]
+    outfilename = osp.join(
+        osp.dirname(__file__),
+        "..",
+        "out",
+        "%s_%s.out" % (output_name, get_time_stamp(with_date=False, with_delims=False)),
+    )
+    key_output_order = ["row", "left_slot", "pool_id"]
+    with open(outfilename, "w") as fout:
+        for alloc in server_allocation:
+            if alloc["left_slot"] == -1:
+                assert alloc["pool_id"] == -1
+                assert alloc["row"] == -1
+                fout.write("x\n")
+            else:
+                fout.write("%s\n" % " ".join([str(alloc[k]) for k in key_output_order]))
+
+
 if __name__ == "__main__":
     print(load(example=True))
+    save(
+        [
+            {"left_slot": -1, "pool_id": -1, "row": -1},
+            {"left_slot": 3, "pool_id": 0, "row": 2},
+        ],
+        output_name="test",
+    )
