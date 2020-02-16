5462a8ac7bedb646cf5e9c92e9f8429775e51193
Christoph Rist <c.rist@posteo.de>
2020-02-16 14:04:10 +0100

    Replace local import with package import

--- src/test_imports_ide.py
+++ src/test_imports_ide.py
@@ -2,7 +2,7 @@
 
 import os.path as osp
 
-from common import readvalues
+from src.common import readvalues
 
 values = readvalues(osp.join(osp.dirname(__file__), "..", ".gitignore"))
 print(values[0])
