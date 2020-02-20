ed2dbd2f5d109e56ee2738000b555830e777eac6
DavidS3141 <davidschmidt314@gmail.com>
2020-02-20 19:39:18 +0100

    first greedy solution

---
+++ src/greedy_most_books.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+
+from common import load, save
+
+
+def compute_nbr_books_processable(lib, days_left: int, books_to_scan_left: set):
+    days_left -= lib["t"]
+    max_books = days_left * lib["m"]
+    interesting_books_in_lib = lib["ids"] & books_to_scan_left
+    result = min(max_books, len(interesting_books_in_lib))
+    print("nbr books processable", result)
+    return result
+
+
+def get_lib_to_output(lib, days_left: int, books_to_scan_left: set, scores: list):
+    days_left -= lib["t"]
+    max_books = days_left * lib["m"]
+    interesting_books_in_lib = lib["ids"] & books_to_scan_left
+    logged_books = sorted(interesting_books_in_lib, key=lambda idx: -scores[idx])[
+        :max_books
+    ]
+    books_to_scan_left -= set(logged_books)
+    return {"index": lib["index"], "ids": logged_books}
+
+
+def main(ds_name="a_example"):
+    data = load(ds_name)
+    num_books_added_by_last_lib = 1
+
+    books_left = set(range(len(data["S"])))
+    unused_libs = list(data["libs"])
+    output = []
+    days_left = data["D"]
+    scores = data["S"]
+    while num_books_added_by_last_lib > 0 and len(unused_libs) > 0:
+        sel_lib = max(
+            unused_libs,
+            key=lambda lib: compute_nbr_books_processable(
+                lib, days_left=days_left, books_to_scan_left=books_left
+            ),
+        )
+        del unused_libs[unused_libs.index(sel_lib)]
+        output.append(
+            get_lib_to_output(
+                sel_lib,
+                days_left=days_left,
+                books_to_scan_left=books_left,
+                scores=scores,
+            )
+        )
+        days_left -= sel_lib["t"]
+        print("sel_lib", sel_lib)
+        print("days_left", days_left)
+        num_books_added_by_last_lib = len(output[-1]["ids"])
+        print("num_books_added_by_last_lib", num_books_added_by_last_lib)
+    save(output, method_name="greedy_most_books", ds_name=ds_name)
+
+
+if __name__ == "__main__":
+    from src.solution_super_simple import dsets
+
+    for dset in dsets:
+        main(dset)
--- src/solution_super_simple.py
+++ src/solution_super_simple.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
 
-from src.common import load, save, score, load_output
+from src.common import load, load_output, save, score
 
 
 def solution_super_simple(data_input):
-    return {'libs': []}
+    return {"libs": []}
 
+
+dset_a = "a_example"
+dset_b = "b_read_on"
+dset_c = "c_incunabula"
+dset_d = "d_tough_choices"
+dset_e = "e_so_many_books"
+dset_f = "f_libraries_of_the_world"
+dsets = [dset_a, dset_b, dset_c, dset_d, dset_e, dset_f]
 
 if __name__ == "__main__":
-
-    dset_a = "a_example"
-    dset_b = "b_read_on"
-    dset_c = "c_incunabula"
-    dset_d = "d_tough_choices"
-    dset_e = "e_so_many_books"
-    dset_f = "f_libraries_of_the_world"
-    dsets = [dset_a, dset_b, dset_c, dset_d, dset_e, dset_f]
 
     # dsets = dsets[0:1]
 
@@ -25,7 +25,9 @@ if __name__ == "__main__":
         data_input = load(dset)
         print(data_input)
         solution_output = solution_super_simple(data_input=data_input)
-        score_value, filename = save(solution_output, method_name="method", ds_name=dset)
+        score_value, filename = save(
+            solution_output, method_name="method", ds_name=dset
+        )
         print(score(solution_output, data_input))
 
         total_score += score_value
