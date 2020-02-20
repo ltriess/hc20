b63df6b74d8a3c05a64e0344fa8b8b7399bce3bf
Christoph Rist <c.rist@posteo.de>
2020-02-20 18:42:52 +0100

    Super simple solution

--- src/solution_super_simple.py
+++ src/solution_super_simple.py
@@ -8,12 +8,29 @@ def solution_super_simple(data_input):
 
 
 if __name__ == "__main__":
-    dset = "example"
 
-    data_input = load(dset)
-    print(data_input)
-    solution_output = solution_super_simple(data_input=data_input)
-    score_value, filename = save(solution_output, method_name="method", ds_name=dset)
-    print(score(solution_output, data_input))
-    solution_output = load_output(filename)
-    print(solution_output)
+    # dset = "example"
+    dset_a = "a_example"
+    dset_b = "b_small"
+    dset_c = "c_medium"
+    dset_d = "d_quite_big"
+    dset_e = "e_also_big"
+    dsets = [dset_a, dset_b, dset_c, dset_d, dset_e]
+
+    dsets = dsets[0:1]
+
+    total_score = 0
+
+    for dset in dsets:
+        data_input = load(dset)
+        print(data_input)
+        solution_output = solution_super_simple(data_input=data_input)
+        score_value, filename = save(solution_output, method_name="method", ds_name=dset)
+        print(score(solution_output, data_input))
+
+        total_score += score_value
+
+        # solution_output = load_output(filename)
+        # print(solution_output)
+
+    print(total_score)
